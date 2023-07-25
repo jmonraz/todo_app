f0edd99a1fe754d6a27d148a0187e53f9394b033
jorgemonraz <jmonraz@student.fullsail.edu>
2023-07-25 11:01:44 -0700

    added update button to the todo task field

--- src/components/TodoField/TodoField.jsx
+++ src/components/TodoField/TodoField.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styles from './TodoField.module.css';
 
-const TodoField = ({ children, onClick, id }) => {
+const TodoField = ({ children, onClick, id, onUpdate }) => {
     return (
         <>
             <div className={styles.container}>
                 <p className={styles.title}>{children}</p>
+                <button className={styles.update_button} onUpdate={() => onUpdate(id)}>Update</button>
                 <button className={styles.delete_button} onClick={() => onClick(id)}>Delete</button>
             </div>
         </>
